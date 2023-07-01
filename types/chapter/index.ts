ae8e406a8dd3c651d5575d6a63374348cdb3c437
quochuy2405 <huy01682312920@gmail.com>
2023-07-01 19:29:43 +0700

    update

--- types/chapter/index.ts
+++ types/chapter/index.ts
@@ -1,7 +1,5 @@
-import { IMyDocument } from "..";
-
 export type IChapter = {
-  course_id: Schema.Types.ObjectId;
-  chapter_name: string;
-  course_name: string;
-} & IMyDocument;
+  course_id: Schema.Types.ObjectId
+  chapter_name: string
+  course_name: string
+}
