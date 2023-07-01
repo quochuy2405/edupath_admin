5fc94d251ec2be91795dc17c0b38c63c7b6abbce
quochuy2405 <huy01682312920@gmail.com>
2023-07-01 19:23:59 +0700

    update

--- apis/detail/index.ts
+++ apis/detail/index.ts
@@ -1,15 +1,14 @@
-import { IFormik } from "~/components/organisms/detail/Add";
-import AxiosServices from "../axiosServices";
-import { IDetail } from "~/types/detail";
+import { TDetails } from '@/types/common'
+import AxiosServices from '../axiosServices'
 
-export const addDetail = (data: IFormik) => {
-  return new AxiosServices().post(`admin/detail/add`, data);
-};
+export const addDetail = (data: any) => {
+  return new AxiosServices().post(`admin/detail/add`, data)
+}
 
 export const allDetails = () => {
-  return new AxiosServices().get<IDetail[]>(`admin/detail/all`);
-};
+  return new AxiosServices().get<TDetails[]>(`admin/detail/all`)
+}
 
 export const removeDetail = (_id: string) => {
-  return new AxiosServices().delete<IDetail>(`admin/detail/remove/${_id}`);
-};
+  return new AxiosServices().delete<TDetails>(`admin/detail/remove/${_id}`)
+}
